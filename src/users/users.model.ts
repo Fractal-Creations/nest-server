6b70d3fea2ca8d5c0ad87c6f94888fa396c9558c
totalfractal <an.tyrtyshnikov@gmail.com>
2022-07-30 23:28:50 +0300

    - добавлены регистрация и авторизация с использованием jwt - хэширование паролей - дополнена документация API

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -33,6 +33,6 @@ export class User extends Model<User, UserCreationAttrs>{
     banReason: string;
 
     @BelongsToMany(() => Role, () => UserRoles)
-    users: Role[];
+    roles: Role[];
 
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthService } from 'src/auth/auth.service';
 import { Role } from 'src/roles/roles.model';
+import { RolesModule } from 'src/roles/roles.module';
+import { RolesService } from 'src/roles/roles.service';
 import { BannedUser } from './banned-users.model';
 import { UsersController } from './users.controller';
 import { User } from './users.model';
@@ -10,7 +13,9 @@ import { UsersService } from './users.service';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-    SequelizeModule.forFeature([User, BannedUser, Role])
-  ]
+    SequelizeModule.forFeature([User, BannedUser, Role]),
+    RolesModule,
+  ],
+  exports: [UsersService]
 })
 export class UsersModule {}
