91dcab33ab9718a4e3eaa5df03e142fbd15259ce
Andrey Tyrtyshnikov <an.tyrtyshnikov@gmail.com>
2022-10-15 15:02:54 +0300

    - enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,23 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { JwtService } from "@nestjs/jwt";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import {AppModule} from "./app.module";
+import { AppModule } from "./app.module";
 import { CustomValidationPipe } from "./pipes/validation.pipe";
 
 
 async function start() {
     console.log(process.env.NODE_ENV);
-    
+
     const PORT = process.env.PORT || 5000
     const app = await NestFactory.create(AppModule, {
-        logger: ['log','error', 'warn', 'debug'],
-    })
-    app.useGlobalPipes(new CustomValidationPipe);
+        logger: ['log', 'error', 'warn', 'debug'],
+    });
+    app.useGlobalPipes(new CustomValidationPipe)
+        .enableCors({
+            origin: true,
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+            credentials: true,
+        });
     //app.useGlobalPipes(new ValidationPipe);
 
     const config = new DocumentBuilder()
@@ -22,12 +27,12 @@ async function start() {
         .setVersion('0.0.3')
         .addBearerAuth()
         .build()
-        
+
 
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('api/docs', app, document)
-    
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+
+    await app.listen(PORT, () => console.log(`Server started on port = ${ PORT }`))
 
 }
 
